ac78f4cd550d33e9103dc6cccc860e4793d457db
Sneaky Koder <sneaky.koder@gmail.com>
2021-01-08 19:08:17 +0000

    Docker updates

--- docker-dev-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo rm -rf node_modules/
-docker build -f Dockerfile.dev -t home-dashboard-react:dev .
-#docker run -it -p 3000:3000 -v $(pwd):/app home-dashboard-react:dev
-docker run -it -p 3000:3000 home-dashboard-react:dev
