68b45703bb7b521803085e902d0aea25ac1a13ee
Sneaky Koder <sneaky.koder@gmail.com>
2021-12-23 18:42:51 -0700

    Weather Complementary component updated

--- src/components/WeatherCurrentComp/WeatherCurrentComp.js
+++ src/components/WeatherCurrentComp/WeatherCurrentComp.js
@@ -3,22 +3,42 @@ import React from 'react';
 export default function WeatherCurrentComp(props){
     return (
         <div className="weatherCurrentComp ">
-            <div className="weatherCurrentComp_item border-right">
+            <div className="weatherCurrentComp_item border-right border-bottom">
                 <div>UV: {props.weather.uv.index}</div>
-                <div> {props.weather.uv.text}</div>
+                {
+                    props.weather.uv.index > 0 ? 
+                    (<div> {props.weather.uv.text} </div>) : <></>
+                }
             </div>
             <div className="weatherCurrentComp_item border-bottom">
                 <div>Humidity:</div>
-                <div>{props.weather.humidity} %</div>
+                {
+                    props.weather.humidity > 0 ?
+                    ( <div>{props.weather.humidity} %</div> ) : <></>
+                }
             </div>
-            <div className="weatherCurrentComp_item border-top">
+            <div className="weatherCurrentComp_item border-right border-bottom">
                 <div>Pressure:</div>
                 <div>{props.weather.pressure.value} {props.weather.pressure.unit}</div>
             </div>
-            <div className="weatherCurrentComp_item border-left">
+            <div className="weatherCurrentComp_item border-bottom">
                 <div>Wind:</div>
-                <div>{props.weather.wind.speed.value} {props.weather.wind.speed.unit}</div>
-                <div>{props.weather.wind.direction}</div>
+                {
+                    props.weather.wind.speed.value > 0 ?
+                    (<>
+                        
+                        <div>{props.weather.wind.speed.value} {props.weather.wind.speed.unit} {props.weather.wind.direction}</div>
+                    </>) : <></>
+                }
+                
+            </div>
+            <div className="weatherCurrentComp_item border-right">
+                <div>Sunrise:</div>
+                <div>{props.sunRise}</div>
+            </div>
+            <div className="weatherCurrentComp_item">
+                <div>Sunset:</div>
+                <div>{props.sunSet}</div>
             </div>
         </div>
     );
