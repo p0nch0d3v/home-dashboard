b97a7bc8831e706d894eb84ec5a50eadeec891d6
Sneaky Koder <sneaky.koder@gmail.com>
2021-12-28 10:50:57 -0700

    Weather complementary border added

--- src/components/WeatherCurrentComp/WeatherCurrentComp.js
+++ src/components/WeatherCurrentComp/WeatherCurrentComp.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 export default function WeatherCurrentComp(props){
     return (
-        <div className="weatherCurrentComp ">
-            <div className="weatherCurrentComp_item  ">
+        <div className="weatherCurrentComp">
+            <div className="weatherCurrentComp_item border-right border-bottom">
                 <div>UV: {props.weather.uv.index}</div>
                 {
                     props.weather.uv.index > 0 ? 
                     <div>{props.weather.uv.text}</div> : <></>
                 }
             </div>
-            <div className="weatherCurrentComp_item ">
+            <div className="weatherCurrentComp_item border-right border-bottom">
                 <div>Humidity:</div>
                 {
                     props.weather.humidity > 0 ?
                     <div>{props.weather.humidity} %</div> : <></>
                 }
             </div>
-            <div className="weatherCurrentComp_item  ">
+            <div className="weatherCurrentComp_item border-bottom">
                 <div>Pressure:</div>
                 <div>{props.weather.pressure.value} {props.weather.pressure.unit}</div>
             </div>
-            <div className="weatherCurrentComp_item ">
+            <div className="weatherCurrentComp_item border-right">
                 <div>Wind:</div>
                 {
                     props.weather.wind.speed.value > 0 ?
@@ -31,7 +31,7 @@ export default function WeatherCurrentComp(props){
                     </>) : <></>
                 }
             </div>
-            <div className="weatherCurrentComp_item ">
+            <div className="weatherCurrentComp_item border-right">
                 <div>Sunrise:</div>
                 <div>{props.sunRise}</div>
             </div>
