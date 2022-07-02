990b3493d0077fcf72ed979e950711e32ae23529
p0nch0 d3v <p0nch0.d3v@gmail.com>
2022-07-02 13:44:31 -0700

    Day Of Year moved to calendar

--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './DateTime.scss';
 import { useTranslation, withTranslation } from 'react-i18next';
 
-export default function DateTime({ time, weekDay, date, dayOfYear, remainingDaysOfYear }){
-    const { t } = useTranslation();
-
+export default function DateTime({ time, weekDay, date }){
     return (
         <div className="datetime">
             <div className="time">{time}</div>
             <div className="weekDay">{weekDay}{' '}</div>
             <div className="date">{date}</div>
-            <div className="dayOfYear">{t("Day")} {dayOfYear}, {t("Remaining")} {remainingDaysOfYear} </div>
         </div>
     )
 }
