968da8924e3bd7ccaa07f5952b68b2f8b3924d89
p0nch0 d3v <p0nch0.d3v@gmail.com>
2022-07-06 11:12:57 -0700

    date time shuffle items

--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './DateTime.scss';
-import { useTranslation, withTranslation } from 'react-i18next';
+import { shuffle_array } from '../../helpers'
 
 export default function DateTime({ time, weekDay, date }){
     return (
         <div className="datetime">
-            <div className="time">{time}</div>
-            <div className="weekDay">{weekDay}{' '}</div>
-            <div className="date">{date}</div>
+            { 
+                shuffle_array([
+                    <div className="time">{time}</div>,
+                    <div className="weekDay">{weekDay}{' '}</div>,
+                    <div className="date">{date}</div>
+                ]).map((v, i) => {
+                    return v    
+            })
+            }
         </div>
     )
 }
