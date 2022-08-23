e599ac908a4313fd0b91de09b28e4e8253e8e165
p0nch0 d3v <p0nch0.d3v@gmail.com>
2022-08-23 12:27:01 -0700

    WIP, styling updates

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -2,31 +2,35 @@ import React from 'react';
 import './MainHeader.scss';
 
 export default function MainHeader({ date, time, temp, feelTemp, iconCode, onTouchEnd, className }){
+    const items = [];
+    if (time) {
+        items.push(<div className="time">{time}</div>);
+    }
+    if (temp && feelTemp && iconCode) {
+        items.push( 
+            <div className="temp">
+            {
+                iconCode ? <span style={{marginRight: '0.5rem'}} className={'icon ' + iconCode}></span>
+                : <></>
+            }
+            <span>{temp}</span>
+            {feelTemp !== temp ? <span>&nbsp;({feelTemp})</span> : <></>}
+            </div>
+        );
+    }
+    if (date) {
+        items.push(<div className="date">{date}</div>);
+    }
+    
     return (
         <div className={ className +  ' main-header' }
             onTouchEnd={onTouchEnd}>
-            {
-                time ? (
-                    <div className="time">{time}</div>
-                ) : <></>
-            }
-            {
-                temp && feelTemp && iconCode ? (
-                    <div className="temp">
-                        {
-                            iconCode ? <span style={{marginRight: '0.5rem'}} className={'icon ' + iconCode}></span>
-                            : <></>
-                        }
-                        <span>{temp}</span>
-                        {feelTemp !== temp ? <span>&nbsp;({feelTemp})</span> : <></>}
-                    </div>
-                ): <></>
-            }
-            {
-                date ? (
-                    <div className="date">{date}</div>
-                ) : <></>
-            }
+             { items.map((value, index) => {
+                return <>
+                        {value}
+                        {value && items.length > 1 && (index + 1) < items.length ? '|' : ''}
+                    </>
+              }) }
          </div>
     )
 }
