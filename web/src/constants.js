6a1a5a5d090d64a83484f3c7345de2d8076d07b5
p0nch0 d3v <p0nch0.d3v@gmail.com>
2023-04-13 17:04:04 -0700

    last tweet by config updates

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,11 +1,21 @@
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
+
 export const Times = {
-    second: 1000,
-    minute: 1000 * 60,
-    hour: (1000 * 60) * 60
+    second: second,
+    minute: minute,
+    hour: hour,
+    day: day,
+    week: week
 };
 
 export const TimeNames = {
     second: "second",
     minute: "minute",
-    hour: "hour"
+    hour: "hour",
+    day: "day",
+    week: "week"
 };
